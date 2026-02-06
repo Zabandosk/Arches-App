21d268c217e64c495dd63cd86c4555e27389d185
zabandosk <orhun.ugur93@gmail.com>
2026-02-06 18:06:34 +0000

    getting json on report page

--- arches_slocal/arches_slocal/media/js/reports/orhuns_report.js
+++ arches_slocal/arches_slocal/media/js/reports/orhuns_report.js
@@ -3,11 +3,32 @@ define([
     'viewmodels/report',
     'templates/views/report-templates/orhuns_report.htm'
 ], function(ko, ReportViewModel, customReportTemplate) {
+
     return ko.components.register('orhuns_report', {
         viewModel: function(params) {
-            params.configKeys = []; 
-            ReportViewModel.apply(this, [params]);
+            var self = this;
+
+
+            self.report = params.report;
+
+
+            self.reportDate = self.report.report_json ? self.report.report_json.report_date : 'No date';
+            self.firstCardName = self.report.cards && self.report.cards.length > 0 ? self.report.cards[0].name : 'No cards';
+            self.hasProvisionalData = function() { return false; };
+            self.summary = null;
+            self.configForm = null;
+
+            console.log('==== ORHUNS REPORT ====');
+            console.log('report object:', self.report);
+
+            // Tiles log (value varsa)
+            if (self.report.attributes && self.report.attributes.tiles) {
+                self.report.attributes.tiles.forEach(function(tile, i) {
+                    console.log('Tile', i, 'tileid:', tile.tileid, 'nodegroup_id:', tile.nodegroup_id);
+                    console.log('  data:', tile.data); 
+                });
+            }
         },
-        template: customReportTemplate,
+        template: customReportTemplate
     });
 });
